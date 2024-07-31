92f58c6076fe9ee30cc0f0c93c9fe35bbcba85ed
ediazva <ediazva@unsa.edu.pe>
2024-07-31 13:48:25 -0500

    yape

---
+++ scripts/items.js
@@ -0,0 +1,11 @@
+Array.from(document.getElementsByTagName("input")).filter(x => x.type === "tel").forEach(x => {
+  x.addEventListener("input", just_number);
+});
+
+/**
+ * 
+ * @param {Event} ev 
+ */
+function just_number(ev) {
+  ev.target.value = ev.target.value.replace(/[^\d]/g,'');
+}
